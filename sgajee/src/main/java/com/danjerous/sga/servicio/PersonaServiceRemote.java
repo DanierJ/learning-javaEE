f8414d1e204d5b8e25d66f335b08f79cbca0f418
DanierJ <danierjavid@gmail.com>
2019-08-06 17:58:32 -0500

    Agregando la interfaz remota

---
+++ sgajee/src/main/java/com/danjerous/sga/servicio/PersonaServiceRemote.java
@@ -0,0 +1,21 @@
+package com.danjerous.sga.servicio;
+
+import com.danjerous.sga.domain.Persona;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+/**
+ * Nos permitirá mandar a llamar los métodos del ejb
+ */
+@Remote // Para convertirla en una interface remota
+public interface PersonaServiceRemote {
+
+    List<Persona> listarPersonas();
+    void encontrarPersonaPorId(Persona persona);
+    void encontrarPersonaPorEmail(Persona persona);
+
+    void registrarPersona(Persona persona);
+    void modificarPersona(Persona persona);
+    void eliminarPersona(Persona persona);
+}
