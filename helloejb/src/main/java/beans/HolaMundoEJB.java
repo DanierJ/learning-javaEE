8daf284027822bd5a702687bd460db93f28767cf
DanierJ <danierjavid@gmail.com>
2019-08-06 15:09:42 -0500

    Agregando la estructura de los test

--- helloejb/src/main/java/beans/HolaMundoEJB.java
+++ helloejb/src/main/java/beans/HolaMundoEJB.java
@@ -2,6 +2,16 @@ package beans;
 
 import javax.ejb.Stateless;
 
-@Stateless
+@Stateless // Tan sólo poniendo esta anotación la clase se convierte en un EJB
 public class HolaMundoEJB {
+
+    /**
+     * Definiendo nuestro método de negocio. Este método ya posee todas las características empresariales de los ejb. EJB de tipo local sin interface
+     * @param a
+     * @param b
+     * @return suma de a y b
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
 }
