f4a3f01f1398fb619a8509b0fcd35e3f45f22df6
DanierJ <danierjavid@gmail.com>
2019-08-06 18:37:17 -0500

    Terminando el cliente

--- sgajee/src/main/java/com/danjerous/sga/cliente/ClientePersonaService.java
+++ sgajee/src/main/java/com/danjerous/sga/cliente/ClientePersonaService.java
@@ -1,7 +1,34 @@
 package com.danjerous.sga.cliente;
 
+import com.danjerous.sga.domain.Persona;
+import com.danjerous.sga.servicio.PersonaServiceRemote;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.List;
+
+
 public class ClientePersonaService {
     public static void main(String[] args) {
-        
+
+        System.out.println("Iniciando llamada al EJB desde el cliente\n");
+
+        try {
+            Context jndi = new InitialContext();
+            PersonaServiceRemote personaService =  (PersonaServiceRemote) jndi.lookup("java:global/sga-jee/PersonaServiceImpl!com.danjerous.sga.servicio.PersonaServiceRemote");
+
+            List<Persona> listadoPersona = personaService.listarPersonas();
+
+            listadoPersona.forEach(System.out::println);
+
+
+            System.out.println("\nFin llamada al EJB desde el cliente.");
+
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
