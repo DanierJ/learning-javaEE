6aca5bcc22e043fe8d42b066b8e234f3de58e78b
DanierJ <danierjavid@gmail.com>
2019-08-06 18:07:56 -0500

    Hardcoding el listado de personas

--- sgajee/src/main/java/com/danjerous/sga/servicio/PersonaServiceImpl.java
+++ sgajee/src/main/java/com/danjerous/sga/servicio/PersonaServiceImpl.java
@@ -3,13 +3,25 @@ package com.danjerous.sga.servicio;
 import com.danjerous.sga.domain.Persona;
 
 import javax.ejb.Stateless;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class PersonaServiceImpl implements PersonaServiceRemote {
     @Override
     public List<Persona> listarPersonas() {
-        return null;
+
+
+        List<Persona> listadoPersona = new ArrayList<>();
+
+        listadoPersona.add(new Persona(1, "Danier", "Martinez", "Camacho", "dan@j.com", "222555"));
+        listadoPersona.add(new Persona(2, "Julian", "Cuestas", "Mora", "julian@j.com", "222555"));
+        listadoPersona.add(new Persona(3, "Mario", "Vasquez", "joderSeMeOlvido", "mar@v.com", "222555"));
+        listadoPersona.add(new Persona(4, "Poncho", "??", "???", "ponchis", "222555"));
+        listadoPersona.add(new Persona(5, "Isaac", "Amortegui", "Cuervo", "isaac.com", "222555"));
+
+        return listadoPersona;
+
     }
 
     @Override
