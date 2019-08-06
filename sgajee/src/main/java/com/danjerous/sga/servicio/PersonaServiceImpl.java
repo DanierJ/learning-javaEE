a0291e300efe2ed26efcb187c0b6411f9d453027
DanierJ <danierjavid@gmail.com>
2019-08-06 17:59:46 -0500

    Agregando el cuerpo de la implementación

---
+++ sgajee/src/main/java/com/danjerous/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,37 @@
+package com.danjerous.sga.servicio;
+
+import com.danjerous.sga.domain.Persona;
+
+import java.util.List;
+
+public class PersonaServiceImpl implements PersonaServiceRemote {
+    @Override
+    public List<Persona> listarPersonas() {
+        return null;
+    }
+
+    @Override
+    public void encontrarPersonaPorId(Persona persona) {
+
+    }
+
+    @Override
+    public void encontrarPersonaPorEmail(Persona persona) {
+
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+
+    }
+}
