0183de964950e0b498f63b1c4122565e9599dbdf
DanierJ <danierjavid@gmail.com>
2019-08-08 17:53:29 -0500

    Prueba unitaria superada

---
+++ sgajee/target/generated-sources/annotations/com/danjerous/sga/domain/Persona_.java
@@ -0,0 +1,18 @@
+package com.danjerous.sga.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.4.v20190115-rNA", date="2019-08-08T17:10:05")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, String> apeMaterno;
+    public static volatile SingularAttribute<Persona, String> telefono;
+    public static volatile SingularAttribute<Persona, String> nombre;
+    public static volatile SingularAttribute<Persona, String> apePaterno;
+    public static volatile SingularAttribute<Persona, Integer> idPersona;
+    public static volatile SingularAttribute<Persona, String> email;
+
+}
