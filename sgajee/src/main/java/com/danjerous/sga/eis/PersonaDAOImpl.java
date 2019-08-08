6394477445e92463331255e95423aae289f435cd
DanierJ <danierjavid@gmail.com>
2019-08-08 16:31:20 -0500

    Agregando la implementación de PersonaDAO

---
+++ sgajee/src/main/java/com/danjerous/sga/eis/PersonaDAOImpl.java
@@ -0,0 +1,37 @@
+package com.danjerous.sga.eis;
+
+import com.danjerous.sga.domain.Persona;
+
+import java.util.List;
+
+public class PersonaDAOImpl implements PersonaDAO {
+    @Override
+    public List<Persona> findAllPersonas() {
+        return null;
+    }
+
+    @Override
+    public Persona findPersonaById(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona findPersonaByEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void insertPersona(Persona persona) {
+
+    }
+
+    @Override
+    public void updatePersona(Persona persona) {
+
+    }
+
+    @Override
+    public void deletePersona(Persona persona) {
+
+    }
+}
