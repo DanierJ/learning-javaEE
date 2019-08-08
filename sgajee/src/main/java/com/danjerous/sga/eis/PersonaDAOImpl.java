faa6df04fe77ce0f232894414a2dd8df10fac93e
DanierJ <danierjavid@gmail.com>
2019-08-08 16:43:27 -0500

    Implementada la capa de datos

--- sgajee/src/main/java/com/danjerous/sga/eis/PersonaDAOImpl.java
+++ sgajee/src/main/java/com/danjerous/sga/eis/PersonaDAOImpl.java
@@ -2,36 +2,50 @@ package com.danjerous.sga.eis;
 
 import com.danjerous.sga.domain.Persona;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
+@Stateless
 public class PersonaDAOImpl implements PersonaDAO {
+
+    @PersistenceContext(unitName = "PersonaPU")
+    EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
     @Override
     public List<Persona> findAllPersonas() {
-        return null;
+        return entityManager.createNamedQuery("Persona.findAll").getResultList();
     }
 
     @Override
     public Persona findPersonaById(Persona persona) {
-        return null;
+       return entityManager.find(Persona.class, persona.getIdPersona());
     }
 
     @Override
     public Persona findPersonaByEmail(Persona persona) {
-        return null;
+        Query query = entityManager.createQuery("FROM Persona p WHERE p.email = :email");
+        query.setParameter("email", persona.getEmail());
+
+        return (Persona) query.getSingleResult();
     }
 
     @Override
     public void insertPersona(Persona persona) {
-
+        entityManager.persist(persona);
     }
 
     @Override
     public void updatePersona(Persona persona) {
-
+        entityManager.merge(persona);
     }
 
     @Override
     public void deletePersona(Persona persona) {
-
+        entityManager.merge(persona);
+        entityManager.remove(persona);
     }
 }
